3f894522fda239a9d3b853d19f08e37aaaf679b2
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-11-04 14:52:24 +0000

    started project

---
+++ application/src/Components/Header/MenuList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const MenuList = () => {
+    return (
+        <div>
+            <ul>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MenuList
