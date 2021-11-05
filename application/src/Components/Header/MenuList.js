105ba2d0b1ca797077f911835392ab1365e96f3a
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-11-05 13:59:28 +0000

    updated

--- application/src/Components/Header/MenuList.js
+++ application/src/Components/Header/MenuList.js
@@ -4,20 +4,15 @@ const MenuList = () => {
     return (
         <div>
             <ul>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
-                <li>List Item</li>
+                <li><a href="/">Fragrances</a></li>
+                <li><a href="/">Men</a></li>
+                <li><a href="/">Women</a></li>
+                <li><a href="/">Gifts</a></li>
+                <li><a href="/">Home & Beauty</a></li>
+                <li><a href="/">Collection</a></li>
+                <li><a href="/">Offers</a></li>
+                <li><a href="/">World of Sunnamusk</a></li>
+                <li><a href="/">Find Your Fragrance</a></li>
             </ul>
         </div>
     )
--- application/src/Components/ProductSection/Product.js
+++ application/src/Components/ProductSection/Product.js
@@ -2,9 +2,46 @@ import React from 'react'
 
 const Product = () => {
     return (
-        <div>
-            <h1>This is the product section</h1>
-        </div>
+        <>
+            <div classNameName="product__wrapper">
+
+                <div classNameName="product__logo">
+                    <h1>Logo Section</h1>
+                </div>
+
+                <div classNameName="product__image">
+                    <h1>Product Image</h1>
+                </div>
+
+                <div classNameName="product__purchase">
+                    <h1>£50.00 or 4 payments of £12.50 with Clearpay</h1>
+
+                    <p>Devastatingly elegant, this fragrance captivates with its woody base, floral heart and notes of citrus to culminate in our top selling fragrance.</p>
+
+                    <div className="form-fields">
+                        <div className="dynamic-select-field">
+                        <label  for="p-quantity">QTY: <span> 1</span> <i className="far fa-chevron-down"></i></label>
+                        <select id="fet-quantity">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        </select>
+                        <div className="js-div"><div value="1" data-value="1">1</div><div value="2" data-value="2">2</div><div value="3" data-value="3">3</div><div value="4" data-value="4">4</div><div value="5" data-value="5">5</div></div></div>
+                        <div className="dynamic-select-field fet-dynamic-size">
+                        <label >SIZE: <span>100ML</span> <i className="far fa-chevron-down"></i></label>
+                        <select id="fet-variation">
+                        <option data-slug="100-ml" value="100-ml">100ML</option>
+                        </select>
+                        <div className="js-div"><div data-slug="100-ml" value="100-ml" data-value="100-ml">100ML</div></div></div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </>
     )
 }
 
